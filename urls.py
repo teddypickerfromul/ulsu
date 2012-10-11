67d525b07808f8e34f6efe63142c8a085668f3e8
teddypickerfromul <teddypickerfromul@gmail.com>
2012-10-12 00:54:45 +0400

    Enabled django admin

--- urls.py
+++ urls.py
@@ -10,10 +10,10 @@ urlpatterns = patterns('',
     # url(r'^ulsu/', include('ulsu.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 # Uncomment these two lines to enable your static files on PythonAnywhere
